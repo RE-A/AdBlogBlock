625aabb99cc44ed043a9dd6e2a35193c93237e96
RE-A <kingsong95@naver.com>
2019-05-17 04:41:13 +0900

    TestModule Fixed
     프로토타입으로 제목만 파싱해오게 만들었음.

--- TestModule/crawl.py
+++ TestModule/crawl.py
@@ -4,7 +4,7 @@ from CrawlFunction import *
 # 검색어는 여기다가 입력
 searchWord = "강남 미용실" 
 #  몇 개의 글을 검색할 것인가? 10 단위로 가능.
-MAX_NUMBER_TO_SEARCH = 10 
+MAX_NUMBER_TO_SEARCH = 20 
 
 ################################################################
 
@@ -12,11 +12,14 @@ maxnum = int(MAX_NUMBER_TO_SEARCH/10)
 url = setURL(searchWord)
 f = open("TestModule/result.txt", "w", encoding='utf8')
 
-for i in range(1,maxnum):
+for i in range(1,maxnum+1):
     html = getSiteHtml(url)
     results = html.findAll('li',{'class':'sh_blog_top'})
+
     for result in results:
-        title = str(result.dl.dt.a['title'])+'\n'
+        title = result.find('a',{'class':'sh_blog_title _sp_each_url _sp_each_title'})
+        title = title['title']+'\n'
         f.write(title)
 
+    url = "https://search.naver.com/search.naver"+html.find('a',{'class':'next'})['href']
 f.close()
