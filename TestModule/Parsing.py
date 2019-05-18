f3e599a5d4d2c9036e96de04776d5829924225f9
RE-A <kingsong95@naver.com>
2019-05-19 03:00:41 +0900

    Test Module ready to run.

--- TestModule/Parsing.py
+++ TestModule/Parsing.py
@@ -1,12 +1,29 @@
 #web parsing with beautifulsoup
-#from bs4 import BeautifulSoup
 #_*_coding: utf-8 _*_
 from urllib.request import urlopen
 from urllib.error import HTTPError
 import re
 import js2py
-read_file = 'TestModule/result.txt'
+from  CrawlFunction import getSiteHtml
+read_file = 'TestModule/CrawlResult.txt'
 write_file = 'TestModule/parsed.txt'
+
+def garbageCut(texts, isFirst, string):
+    # string is always '본문 기타 기능' or '이 글에 공감한 블로거 열고 닫기' 
+    try:
+        idx = texts.index(string)
+    except ValueError:  #모바일에서 작성한 글일 경우 특수한 처리가 필요함.
+        string = '모바일에서 작성된 글입니다.'
+        idx = texts.index(string)
+    finally:
+        if isFirst is True:
+            texts = texts[idx+1:]
+            return texts
+        else:
+            texts = texts[:(idx-1)]
+            return texts
+
+
 
 def web_parse(url):     # Function of Open HTML.
     try:
@@ -35,7 +52,13 @@ def firstparse(url):    # Fisrt Parsing to find REAL URl (in #document)
     return link
 
 def secoundparse(url):  # Second Parsing of REAL URL
-    pics, stickers, texts, text = [],[],[],[]  # arrays of pic, sticker, text.
+    # For Checking if the html contains Naver Map element
+    html = getSiteHtml(url)
+    result = html.find('iframe',{'title':'포스트에 첨부된 지도'})
+    if result is not None:
+        return
+
+    pics, stickers, texts, text = [], [] ,[], [] # arrays of pic, sticker, text.
     secondhtml = web_parse(url)  
     #pics += re.findall('<img src="(.*?)".*?data-width="(.*?)" data-height="(.*?)".*?class="se-image-resource" />'
                         #, secondhtml) #parse pics number
@@ -43,19 +66,33 @@ def secoundparse(url):  # Second Parsing of REAL URL
                         , secondhtml) #parse pics number
     stickers += re.findall('<img.*? src="(.*?)".*?class="se-sticker-image" />',secondhtml)  #parse sticker number
     text += re.findall('<span.*?>(.*?)</span>',secondhtml) #parse text
-    for parse in text:
-        texts += re.findall("[가-힝 ]",parse)
+
+    # find garbage index : '본문 기타 기능'
+    text = garbageCut(text, True, '본문 기타 기능')
+    text = garbageCut(text, False, '이 글에 공감한 블로거 열고 닫기')
+
+    pattern = re.compile('[ ]?[<].*')
+    for line in text:
+        if pattern.match(line):
+            continue
+        if line in ('','\u200b') :
+            continue
+        else:
+            texts.append(line)
+
+    # for parse in text:
+    #     texts += re.findall("[가-힝 ]",parse)
     f = open(write_file,'a',-1,'utf-8')
-    f.write('====================\n')
-    for text in texts:
-        f.write(text)
+
+    for sentence in texts:
+        f.write(sentence+'\n')
+
     f.write('\n')
     f.write('pics : ')
     f.write(str(len(pics)))
     f.write('\n')
     f.write('stickers : ')
-    f.write(str(len(stickers)))
-    f.write('\n====================\n')
+    f.write(str(len(stickers))+'\n')
     f.close()
 
 def exceptionparse(url):
@@ -84,26 +121,16 @@ def Parse(url):  # url = "https://blog.naver.com/~~~"
 
 # Lines for Simple debugging.
 if __name__ == "__main__":
-    #url = 'http://asnote.net/221502461471'
-    #url = 'http://o-goon.com/221287199411'
-    #url = 'https://m.blog.naver.com/PostView.nhn?blogId=sw4r&logNo=221241276672&proxyReferer=https%3A%2F%2Fwww.google.com%2F'
-    #url = 'https://blog.naver.com/invu1657/221473146800'
-    #url = 'https://blog.naver.com/dragon1086/221037326999'
-    #url = 'https://blog.naver.com/cwy5114/221492558752'
-    #url = 'https://blog.naver.com/my_prayer_s2/221493594784'
-    #url = 'https://blog.naver.com/kmh03214?Redirect=Log&logNo=221434499140'
-    #Parse(url)
-    f = open(read_file,'r')
-    url_num = f.readline()
-    for i in range(0,int(url_num)):
-        url = f.readline()
-        Parse(url)
-    f.close()
 
-"""     BeautifulSoup is too slow to use parsing program
-    for links in link:
-       print(links)
-       html = web_parse(links)
-    soup = BeautifulSoup(html,'html5lib')
-    fixed_html = soup.prettify()
-    print(fixed_html)   """
+    url = 'https://blog.naver.com/epumna/221488195204'
+    Parse(url)
+
+
+
+
+    # f = open(read_file,'r')
+    # url_num = f.readline()
+    # for i in range(0,int(url_num)):
+    #     url = f.readline()
+    #     Parse(url)
+    # f.close()
